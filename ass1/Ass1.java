407e826b0470406f2e57cbc49d6ee239fc14e9fa
Arran France <arranf@users.noreply.github.com>
2015-10-21 23:17:59 +0100

    Initial commit

---
+++ ass1/Ass1.java
@@ -0,0 +1,104 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Ass1{
+	public static void main(String[] args)
+	throws FileNotFoundException {
+		//Relative path to file
+		String fileName = "test.txt";
+		
+		int lineCount = 0;
+		File file = new File(fileName);
+		Scanner counter = new Scanner(file);
+		
+		//Counting
+		while (counter.hasNextInt() ) {
+			counter.nextInt();
+			lineCount++;
+		}
+		counter.close();
+		System.out.println(fileName+"contains "+lineCount+" lines");	
+		
+		//New scanner due to reopening file
+		Scanner scanner = new Scanner(file);
+		
+		int[] buffer = new int[lineCount];
+		int total = 0;
+		int medianPosition, lowerQuartilePosition, upperQuartilePosition;
+		double median, lowerQuartile, upperQuartile;
+		median = lowerQuartile = upperQuartile = 0;
+		
+		//If there is a middle value, set it as the median position otherwise find the highest value of the pair of median values 
+		boolean isOdd = false;
+		if (buffer.length%2 == 1)
+		{
+			isOdd = true;
+		}
+		medianPosition = (int)(Math.ceil((double)buffer.length/2));
+		if (!isOdd)
+		{
+			medianPosition += 1;
+		}
+		System.out.println(medianPosition);
+		lowerQuartilePosition = (int)(Math.ceil((double)medianPosition/2));
+		System.out.println(lowerQuartilePosition);
+		upperQuartilePosition = (int)(Math.ceil((double)buffer.length*0.75));
+		
+		
+		
+		System.out.println(isOdd);
+		
+		for (int i = 0; i < lineCount; i++){
+			buffer[i] = scanner.nextInt();
+			
+			//Check to see if previous value is the same, if so it's a duplicate as the data is ordered
+			if (i > 0 && buffer[i-1] == buffer[i])
+			{
+				System.out.println(buffer[i] + " is a duplicate");
+			}
+			
+			//Find median
+			if (i == (medianPosition-1) && isOdd)
+			{
+				median = buffer[i];
+			}
+			else if (i == (medianPosition-1) && !isOdd)
+			{
+				median = ((double)(buffer[i]+buffer[i-1])/2);
+			}
+			
+			//Find upperQuartile
+			if (i == (upperQuartilePosition-1) && isOdd)
+			{
+				upperQuartile = buffer[i]; 
+			}
+			else if (i == (upperQuartilePosition-1) && !isOdd)
+			{
+				upperQuartile = ((double)(buffer[i]+buffer[i-1])/2);
+			}
+			
+			//Find lowerQuartile
+			if (i == (lowerQuartilePosition-1) && isOdd)
+			{
+				lowerQuartile = buffer[i]; 
+			}
+			else if (i == (lowerQuartilePosition-1) && !isOdd)
+			{
+				lowerQuartile = ((double)(buffer[i]+buffer[i-1])/2);
+			}
+			
+			//Add to total
+			total += buffer[i];
+		}
+		
+		double mean = total/buffer.length;
+		
+		System.out.println("Total: "+total);
+		System.out.println("Mean: "+mean);
+		System.out.println("Median: "+median);
+		System.out.println("UQ: "+upperQuartile);
+		System.out.println("LQ: "+lowerQuartile);
+			
+	}
+}
