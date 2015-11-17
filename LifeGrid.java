85c65e06ec3bf32df3ac8271493417d2f0bef9e8
Arran France <arranf@users.noreply.github.com>
2015-11-17 12:23:56 +0000

    LifeGrid constructor

--- LifeGrid.java
+++ LifeGrid.java
@@ -1,9 +1,52 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 class LifeGrid
 {
-    private int[][] grid;
+  private int[][] grid;
 
-    public LifeGrid(int n, int m)
+  public LifeGrid(int x, int y, String filename)
+  {
+    File file = new File(filename);
+    grid = new int[y][x];
+    int lineCount = 0;
+
+    try
     {
-      grid = new Array[n][m];
+      Scanner scanNumOfLines = new Scanner(file);
+      while (scanNumOfLines.hasNextLine())
+      {
+        lineCount++;
+      }
+      scanNumOfLines.close();
     }
+    catch (FileNotFoundException e)
+    {
+        e.printStackTrace();
+    }
+
+    //Not valid input
+    if (lineCount != y)
+    {
+      //Exception
+    }
+
+    Scanner scanner = new Scanner(file);
+    for (int i = 0; i < y; i++)
+    {
+      String line = scanner.nextLine();
+      for (int j = 0; j < x; j++)
+      {
+        if (line.charAt(i) == '*')
+        {
+          grid[i][j] = 1;
+        }
+        else
+        {
+          grid[i][j] = 0;
+        }
+      }
+    }
+  }
 }
