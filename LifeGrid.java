944a15dd293ee8abd04f936e4aeb0b64380e79f5
Arran France <arranf@users.noreply.github.com>
2015-11-17 12:39:30 +0000

    B.1.2.3

--- LifeGrid.java
+++ LifeGrid.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 class LifeGrid
 {
   private int[][] grid;
+  private int currentGeneration;
 
   public LifeGrid(int x, int y, String filename)
   {
@@ -48,5 +49,57 @@ class LifeGrid
         }
       }
     }
+    currentGeneration = 0;
   }
+
+  public void Show()
+  {
+    System.out.println(currentGeneration);
+    for (int i = 0; i < y; i++)
+    {
+      String line = "";
+      for (int j = 0; j < x; j++)
+      {
+        if (grid[i][j] = 1)
+        {
+          line =+ "*";
+        }
+        else
+        {
+          line =+ " ";
+        }
+        System.out.println(line);
+      }
+    }
+  }
+
+  public int getHeight()
+  {
+    return grid[].length();
+  }
+
+  public int getWidth()
+  {
+    return grid[][].length();
+  }
+
+  public int getCurrentGeneration()
+  {
+    return currentGeneration;
+  }
+
+  public int getCell(int x, int y)
+  {
+    return grid[y][x];
+  }
+
+  public String getCellString(int x, int y)
+  {
+    if (getCell(x, y) == 1)
+      return "*"
+    else
+      return " ";
+  }
+
+
 }
